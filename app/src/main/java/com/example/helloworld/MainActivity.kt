b6b2aaae955b52e9f0521b4ece45d3ea308f7445
aoaojiang <18917944949@189.cn>
2020-03-02 10:51:14 +0800

    2020-03-02

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onB1Clicked(view: View) {
+        textView.text = "The Button has been clicked."
+    }
+}
